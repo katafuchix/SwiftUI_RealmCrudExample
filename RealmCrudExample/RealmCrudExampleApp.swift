c559da96dca333220f0c8431eca170ce745001fd
katafuchix <katafuchix@gmail.com>
2023-01-09 14:44:22 +0900

    Initial Commit

---
+++ RealmCrudExample/RealmCrudExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmCrudExampleApp.swift
+//  RealmCrudExample
+//
+//  Created by cano on 2023/01/09.
+//
+
+import SwiftUI
+
+@main
+struct RealmCrudExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
